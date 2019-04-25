b2fb666369c22845648dd12e573c3917751fe26d
Oleg Tarusov <ot88899888@gmail.com>
2019-04-26 01:27:58 +0300

    folder new-module -> new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers';
 import { NavigationComponent } from './navigation/navigation.component';
-import { NewModule } from './new-module/new-module.module';
-import { EffectsComponent } from './new-module/effects/effects.component';
+import { NewModule } from './new/new-module.module';
+import { EffectsComponent } from './new/effects/effects.component';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
